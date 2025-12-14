1cdaf0c0129629d977b1d53f1219d82f6ca30c91
34lks66 <uranus667788@gmail.com>
2025-12-14 23:07:34 +0100

    export web

--- tp-casse-briques/raquette/controlJoueur.gd
+++ tp-casse-briques/raquette/controlJoueur.gd
@@ -8,7 +8,6 @@ func _ready() -> void:
 	#pass # Replace with function body.
 	screen_size = get_viewport().get_visible_rect().size
 
-
 ## Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta: float) -> void:
 	#pass
