91606b6ddec96b716b3d23b9c8c03e229dafdfe5
34lks66 <uranus667788@gmail.com>
2025-12-16 10:49:02 +0100

    creation mur

--- tp-casse-briques/balle/balle.gd
+++ tp-casse-briques/balle/balle.gd
@@ -1,14 +1,15 @@
 extends RigidBody2D
 
-@export var vitesse_initiale: float = 400.0
+@export var vitesse_initiale: float = 300.0
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
 	linear_velocity = Vector2(0, vitesse_initiale)
 	
-
-
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
+	
+func _physics_process(delta):
+	linear_velocity = linear_velocity.normalized() * vitesse_initiale
